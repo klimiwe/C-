d7373054b2a057724bc7ab3003903a20d895819a
klimiwe <klimiwe@mail.ru>
2022-08-04 19:56:12 +0400

    Example2

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is your name?");
+string username = Console.ReadLine();
+
+Console.WriteLine("Hello, ");
+Console.WriteLine(username);
