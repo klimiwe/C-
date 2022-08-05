5e8291c421ebb03be0c70f18513f7ac441d85f88
klimiwe <klimiwe@mail.ru>
2022-08-05 13:44:41 +0400

    Example5

---
+++ Example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string userName = Console.ReadLine ();
+
+if (userName.ToLower() == "варя"){
+    Console.Write("Ура, это ВАРЯ!!!");
+}
+else 
+{
+    Console.Write("Привет.  ");
+    Console.WriteLine(userName);
+}
